33c70f6286160c154c0a2a753f32600e7204c751
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-11-28 11:51:06 +0530

    added google hash

--- src/utils/googleAuth.js
+++ src/utils/googleAuth.js
@@ -3,7 +3,7 @@ import * as Google from "expo-google-app-auth";
 import { setAuthInfo, setTokenInfo } from "./Auth";
 const _configs = {
   androidClientId: "837990584120-66jqvguk63pcst7tv2e9643pcq7v4s56.apps.googleusercontent.com",
-  androidStandaloneAppClientId: "837990584120-66jqvguk63pcst7tv2e9643pcq7v4s56.apps.googleusercontent.com",
+  androidStandaloneAppClientId: "837990584120-2msdh5tldh1m930es11g24btlohec58u.apps.googleusercontent.com",
   scopes: ["profile", "email"],
 };
 async function googleAuth()
